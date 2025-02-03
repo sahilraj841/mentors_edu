0f94220f82f4f97337672e4526057266e1858674
Siddharth Srivastava <120250296+SiddharthxsrivastavA@users.noreply.github.com>
2025-02-03 23:29:08 +0530

    Update courses.jsx

--- mentors3/src/components/courses.jsx
+++ mentors3/src/components/courses.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect, useRef } from "react"
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/Card"
 import { Button } from "@/components/ui/button"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 
